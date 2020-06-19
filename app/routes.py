17547b70280bb35afe228ed11c151c813fff8014
Andreas Rücklé <andreas.rueckle@arulabs.com>
2020-06-19 15:43:06 +0200

    read model types dynamically from DB

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from flask import Blueprint
-from flask import render_template, request, url_for
-from flask_flatpages import FlatPages
-from .models import Adapter, AdapterType, Model, Task, Subtask
 from itertools import groupby
 
+from flask import Blueprint
+from flask import render_template
+from flask_flatpages import FlatPages
+
+from .models import Adapter, AdapterType, Model, Task, Subtask
 
 bp = Blueprint('main', __name__)
 
@@ -19,10 +20,10 @@ def index():
 @bp.route('/explore/<task_type>/')
 def explore_tasks(task_type='text_task'):
     tasks = Task.query.filter(
-        Task.task_type==task_type if task_type else True
+        Task.task_type == task_type if task_type else True
     ).order_by(Task.displayname).all()
     subtask_query = Subtask.query.filter(
-        Subtask.task_type==task_type if task_type else True
+        Subtask.task_type == task_type if task_type else True
     ).order_by(Subtask.task)
     task_type_ref = AdapterType.query.get(task_type)
     subtasks = {k: list(g) for k, g in groupby(subtask_query, lambda t: t.task)}
@@ -36,19 +37,23 @@ def explore_tasks(task_type='text_task'):
 @bp.route('/explore/<task>/<subtask>/<model_type>/')
 @bp.route('/explore/<task>/<subtask>/<model_type>/<model>/')
 def explore_adapters(task, subtask, model_type=None, model=None):
-    models = Model.query.filter(Model.model_type==model_type if model_type else True)
+    all_model_types = [m.model_type for m in Model.query.distinct(Model.model_type)]
+    all_models = Model.query.filter(Model.model_type == model_type if model_type else True)
     items = Adapter.query.filter(
-                Adapter.task==task,
-                Adapter.subtask==subtask,
-                Adapter.model_type==model_type if model_type else True,
-                Adapter.model_name==model if model else True
-            ).all()
+        Adapter.task == task,
+        Adapter.subtask == subtask,
+        Adapter.model_type == model_type if model_type else True,
+        Adapter.model_name == model if model else True
+    ).all()
     subtask_obj = Subtask.query.get((task, subtask))
     return render_template(
         'explore_adapters.html',
         subtask=subtask_obj,
-        model_type=model_type, model=model,
-        all_models=models, items=items
+        model_type=model_type,
+        all_model_types=all_model_types,
+        model=model,
+        all_models=all_models,
+        items=items
     )
 
 
@@ -56,6 +61,7 @@ def explore_adapters(task, subtask, model_type=None, model=None):
 def adapter_details(groupname, filename):
     adapter = Adapter.query.get((groupname, filename))
     return render_template('adapter.html', adapter=adapter)
+
 
 @bp.route('/upload/', methods=('GET', 'POST'))
 def upload():
